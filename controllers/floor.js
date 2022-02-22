c3d737e282a34df258f317a5052c9700b78c4120
GaryChen <74753438+GaryChen513@users.noreply.github.com>
2022-02-23 03:40:00 +1030

    set up axios

--- controllers/floor.js
+++ controllers/floor.js
@@ -8,6 +8,12 @@ class FloorCtl {
             .catch((err) => res.statu(422).json(err));
     }
 
+    findByLevel(req, res) {
+        Floor.findOne({ floor_level : req.query.level })
+            .then((data) => res.json(data))
+            .catch((err) => res.status(422).json(err));
+    }
+
     update(req, res) {
         Floor.findOneAndUpdate({ _id: req.param.id }, req.body)
             .then((data) => res.json(data))
--- routes/api/floor.js
+++ routes/api/floor.js
@@ -3,11 +3,15 @@ const floorCtl = require("../../controllers/floor");
 
 const {
     findAll,
-    update
+    update,
+    findByLevel
 } = floorCtl;
 
 router.route("/")
     .get(findAll)
+    .get(findByLevel);
+
+router.route("/:id")
     .post(update);
 
 module.exports = router;
