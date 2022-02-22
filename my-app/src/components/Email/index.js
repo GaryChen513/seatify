ec1b7e65c2c0bef2e37d971d16e7e08077395caf
a1779748 <dongwang1998@gmail.com>
2022-02-22 21:23:21 +1030

    update Email/index.js

--- my-app/src/components/Email/index.js
+++ my-app/src/components/Email/index.js
@@ -1,30 +1,66 @@
-import React, { useRef } from 'react';
-import emailjs from '@emailjs/browser';
+import { useState } from 'react';
+import { send } from '@emailjs/browser';
 
 function Email() {
-    const form = useRef();
-  
-    const sendEmail = (e) => {
-      e.preventDefault();
-  
-      emailjs.sendForm('service_61np5dc', 'template_bg695qm', form.current, 'user_UpSYAJoBV5dAAhT9qL1QX')
-        .then((result) => {
-            console.log(result.text);
-        }, (error) => {
-            console.log(error.text);
-        });
+    const [toSend, setToSend] = useState({
+        from_name: '',
+        to_name: '',
+        message: '',
+        reply_to: '',
+      });
+    
+    const onSubmit = (e) => {
+        e.preventDefault();
+        send(
+            'service_gmail',
+            'template_01',
+            toSend,
+            'user_UpSYAJoBV5dAAhT9qL1QX'
+          )
+            .then((response) => {
+              console.log('SUCCESS!', response.status, response.text);
+            })
+            .catch((err) => {
+              console.log('FAILED...', err);
+            });
     };
- 
+
+    const handleChange = (e) => {
+        setToSend({ ...toSend, [e.target.name]: e.target.value });
+    };
+    
     return (
-      <form ref={form} onSubmit={sendEmail}>
-        <label>Name</label>
-        <input type="text" name="user_name" />
-        <label>Email</label>
-        <input type="email" name="user_email" />
-        <label>Message</label>
-        <textarea name="message" />
-        <input type="submit" value="Send" />
-      </form>
+        <form onSubmit={onSubmit}>
+            <input
+            type='text'
+            name='from_name'
+            placeholder='from name'
+            value={toSend.from_name}
+            onChange={handleChange}
+            />
+            <input
+            type='text'
+            name='to_name'
+            placeholder='to name'
+            value={toSend.to_name}
+            onChange={handleChange}
+            />
+            <input
+            type='text'
+            name='message'
+            placeholder='Your message'
+            value={toSend.message}
+            onChange={handleChange}
+            />
+            <input
+            type='text'
+            name='reply_to'
+            placeholder='Your email'
+            value={toSend.reply_to}
+            onChange={handleChange}
+            />
+            <input type="submit" value="Send" />
+        </form>
     );
   }
   
